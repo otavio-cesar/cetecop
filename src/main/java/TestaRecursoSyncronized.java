cbf06abadb455877f2cfb371d15b362f4ff0a413
otavio cesario <otaviool@hotmail.com>
2017-11-07 21:14:18 -0200

    DomjudgeApi

---
+++ src/main/java/TestaRecursoSyncronized.java
@@ -0,0 +1,32 @@
+
+public class TestaRecursoSyncronized extends Thread {
+
+	public static int segundos = 1;
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			new TestaRecursoSyncronized().start();
+		}
+		
+		System.err.println("Fim criacao das 10 threads, mas contando relogio.");
+		
+	}
+
+	private static synchronized void relogio() {
+		System.out.println(segundos++);
+		
+		try {
+			sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void run() {
+		relogio();
+		
+	}
+	
+}
--- src/main/java/service/domjudge/CadastrarProblemaService.java
+++ src/main/java/service/domjudge/CadastrarProblemaService.java
@@ -11,22 +11,45 @@ import model.entidades.Versao;
 @Named
 public class CadastrarProblemaService {
 
-	// TODO Implementar injecao automatica de unidade de persistencia domjudge
+	// TODO Implementar injecao automatica de unidade de persistencia
 	private EntityManager manager;
 
+	private EntityManagerFactory factory;
+
+	public CadastrarProblemaService() {
+		factory = Persistence.createEntityManagerFactory("domjudge");
+	}
+
 	public void guardar(Versao versao, Evento evento) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("domjudge");
 		manager = factory.createEntityManager();
 
-		EntityTransaction trx = this.manager.getTransaction();
-		trx.begin();
-
-		manager.createNativeQuery("INSERT INTO problem (timelimit, name) VALUES (:limit, :name)")
-				.setParameter("limit", 959).setParameter("name", "valdue").executeUpdate();
-
-		trx.commit();
+		inserirProblema(manager, versao, evento);
 
 		manager.close();
+	}
+
+	// Insere um registo por vez, para remover conflito na insercao problemas em um
+	// evento, pois O MAX(probid) utilizado, poderia retornar id's invalidos.
+	private static synchronized void inserirProblema(EntityManager manager, Versao versao, Evento evento) {
+		int problemaId;
+
+		EntityTransaction trx = manager.getTransaction();
+
+		trx.begin();
+		manager.createNativeQuery("INSERT INTO problem (timelimit, name) VALUES (:timelimit, :name)")
+				.setParameter("timelimit", 959).setParameter("name", "valdue").executeUpdate();
+		trx.commit();
+
+		trx.begin();
+		problemaId = (int) manager.createNativeQuery("SELECT MAX(probid) FROM problem").getSingleResult();
+		trx.commit();
+
+		trx.begin();
+		manager.createNativeQuery(
+				"INSERT INTO contestproblem (cid, probid, shortname) VALUES (:cid, :probid, :shortname)")
+				.setParameter("cid", 2).setParameter("probid", problemaId)
+				.setParameter("shortname", "short" + problemaId).executeUpdate();
+		trx.commit();
 
 	}
 }
